de37acc6590d1a01c3fcf4654ccb530fdb922b42
Ap274 <aldopiresaps@gmail.com>
2023-11-22 17:15:39 -0300

    feat: example of receiving candidate information through a POST request

--- src/main/java/br/com/aldopires/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/aldopires/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,15 +1,21 @@
 package br.com.aldopires.gestao_vagas.modules.candidate.controllers;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.aldopires.gestao_vagas.modules.candidate.CandidateEntity;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
     
     @PostMapping("/")
-    public void create() {
+    public void create(@RequestBody CandidateEntity candidateEntity) {
+        System.out.println("Candidato");
+        System.out.println(candidateEntity.getEmail());
+        System.out.println(candidateEntity.getUsername());
 
     }
 }
