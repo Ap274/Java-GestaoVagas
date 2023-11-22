4098ded4a29d40762010b8c0b9d5ee4c795f51ab
Ap274 <aldopiresaps@gmail.com>
2023-11-22 16:47:00 -0300

    feat: creating basic candidate structure + adding lombok dependencie

--- src/main/java/br/com/aldopires/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/aldopires/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package br.com.aldopires.gestao_vagas.modules.candidate.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
     
+    @PostMapping("/")
+    public void create() {
+
+    }
 }
