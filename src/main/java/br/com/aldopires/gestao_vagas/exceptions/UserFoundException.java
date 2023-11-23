983f482c27982ff034f5b302c6d3661eb2e66168
Ap274 <aldopiresaps@gmail.com>
2023-11-23 17:50:35 -0300

    feat: validating existing user

---
+++ src/main/java/br/com/aldopires/gestao_vagas/exceptions/UserFoundException.java
@@ -0,0 +1,8 @@
+package br.com.aldopires.gestao_vagas.exceptions;
+
+public class UserFoundException extends RuntimeException {
+    public UserFoundException() {
+        super("Usuário já existe!");
+    }
+
+}
