f86aee6d440f505ddbdfc894a6d88d5e55291130
Ap274 <aldopiresaps@gmail.com>
2023-12-14 15:40:47 -0300

    fix: removing imports that are not being used

--- src/main/java/br/com/aldopires/gestao_vagas/modules/company/controllers/AuthCompanyController.java
+++ src/main/java/br/com/aldopires/gestao_vagas/modules/company/controllers/AuthCompanyController.java
@@ -1,11 +1,8 @@
 package br.com.aldopires.gestao_vagas.modules.company.controllers;
 
-import javax.naming.AuthenticationException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.config.web.server.ServerHttpSecurity.HttpsRedirectSpec;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
